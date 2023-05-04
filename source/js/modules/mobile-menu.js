00182e661e138e18e8620efef8799367cad1c5aa
Anton Tarasenko <report641@gmail.com>
2023-05-04 16:05:55 +0700

    Правки в соответствии с техническим заданием

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -1,12 +1,23 @@
 const mobileMenu = document.querySelector('[data-mobile-menu]');
 const mobileMenuButton = document.querySelector('[data-mobile-menu-button]');
+const mobilMenuLink = document.querySelectorAll('[data-mobile-menu-link]');
 
 mobileMenuButton.addEventListener('click', ()=>{
   if (mobileMenu.classList.contains('is-open')) {
-    mobileMenu.classList.remove('is-open');
-    window.scrollLock.enableScrolling();
+    onMobilMenuClose();
   } else {
     mobileMenu.classList.add('is-open');
     window.scrollLock.disableScrolling();
+    window.focusLock.lock('.page-header');
   }
+});
+
+const onMobilMenuClose = ()=>{
+  mobileMenu.classList.remove('is-open');
+  window.scrollLock.enableScrolling();
+  window.focusLock.unlock();
+};
+
+mobilMenuLink.forEach((link)=>{
+  link.addEventListener('click', onMobilMenuClose);
 });
