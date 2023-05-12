ffcf2e22ea9224c1171c07f74a94ff972af0abdd
Anton Tarasenko <report641@gmail.com>
2023-05-12 17:43:43 +0700

    Добавлена проверка в скрипт на наличие мобильного меню

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -2,23 +2,27 @@ const mobileMenu = document.querySelector('[data-mobile-menu]');
 const mobileMenuButton = document.querySelector('[data-mobile-menu-button]');
 const mobilMenuLink = document.querySelectorAll('[data-nav-link]');
 
-mobileMenuButton.addEventListener('click', ()=>{
-  if (mobileMenu.classList.contains('is-open')) {
-    onMobilMenuClose();
-    mobilMenuLink.forEach((link)=>{
-      link.removeEventListener('click', onMobilMenuClose);
-    });
-  } else {
-    mobileMenu.classList.add('is-open');
-    window.scrollLock.disableScrolling();
-    window.focusLock.lock('.page-header');
-    if (mobilMenuLink) {
-      mobilMenuLink.forEach((link)=>{
-        link.addEventListener('click', onMobilMenuClose);
-      });
+if (mobileMenu) {
+  mobileMenuButton.addEventListener('click', ()=>{
+    if (mobileMenu.classList.contains('is-open')) {
+      onMobilMenuClose();
+      if (mobilMenuLink) {
+        mobilMenuLink.forEach((link)=>{
+          link.removeEventListener('click', onMobilMenuClose);
+        });
+      }
+    } else {
+      mobileMenu.classList.add('is-open');
+      window.scrollLock.disableScrolling();
+      window.focusLock.lock('.page-header');
+      if (mobilMenuLink) {
+        mobilMenuLink.forEach((link)=>{
+          link.addEventListener('click', onMobilMenuClose);
+        });
+      }
     }
-  }
-});
+  });
+}
 
 const onMobilMenuClose = ()=>{
   mobileMenu.classList.remove('is-open');
